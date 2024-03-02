7921e3b8bbad93da6b327f504559bf9f3b0d842e
Anton Davydovskyi <anton.davydoone@mail.ru>
2024-03-02 19:51:51 +0300

    Deploy RabbitMQ in Docker

--- dispatcher/src/main/java/edu/jcourse/dispatcher/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/edu/jcourse/dispatcher/service/impl/UpdateProducerImpl.java
@@ -1,15 +1,22 @@
 package edu.jcourse.dispatcher.service.impl;
 
 import edu.jcourse.dispatcher.service.UpdateProducer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class UpdateProducerImpl implements UpdateProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.info(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
